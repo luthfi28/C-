862799dc4a2bf9a22b771563fd8320fe83a1280f
Luthfi Athorique Ginanjar Putra <luthfiathorique@gmail.com>
2019-10-03 22:19:38 +0700

    Create Enkripsi_SHA256.cs

---
+++ Enkripsi_SHA256.cs
@@ -0,0 +1,17 @@
+SHA256 encrypt = SHA256.Create();
+string VARIABEL_HASIL_ENKRIPSI = GetHash(encrypt, STRING_YANG_AKAN_DI_ENKRIPSI);
+
+private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+{
+
+    byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+    var sBuilder = new StringBuilder();
+
+    for (int i = 0; i < data.Length; i++)
+    {
+        sBuilder.Append(data[i].ToString("x2"));
+    }
+
+    return sBuilder.ToString();
+}
